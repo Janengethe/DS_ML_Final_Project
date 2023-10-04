8dafb8cc9bd840ac9cd4a2fe25730db149d593d4
Janengethe <janengethew@gmail.com>
2023-10-04 18:50:04 +0300

    streamlit done

--- app.py
+++ app.py
@@ -1,55 +1,66 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 import joblib
+from datetime import datetime
 
 
-# model = joblib.load("best_model.pkl")
+model = joblib.load("best_model.pkl")
 
 
-# Title and Introduction
-st.markdown("<h1 style='color: blue;'>Gym Class Attendance Prediction App</h1>", unsafe_allow_html=True)
-st.markdown("<h2 style='color: blue;'>Predict the number of attendees for gym classes.</h2>", unsafe_allow_html=True)
+def load_model():
+    model = joblib.load("best_model.pkl")
+    return model
 
-# Create a sidebar for user navigation
-st.sidebar.header("Navigation")
-option = st.sidebar.selectbox("Choose an option:", ["Batch Prediction", "Single Row Prediction"])
+model = load_model()
+
+# Streamlit app
+def main():
+    st.title("Gym Class Attendance Predictor")
+    st.write("Enter the following details to predict attendance:")
+
+    # Input fields for relevant features
+    input_date = st.date_input("Date")
+    input_time = st.time_input("Time")
+    temperature = st.slider("Temperature (°F)", 38, 88, 58)
+    is_weekend = st.checkbox("Is Weekend")
+    is_holiday = st.checkbox("Is Holiday")
+    is_start_of_semester = st.checkbox("Is Start of Semester")
+    is_during_semester = st.checkbox("Is During Semester")
+
+    # Extract year, month, date, day of the week, hour, and minutes from input
+    datetime_input = datetime.combine(input_date, input_time)
+    year = datetime_input.year
+    month = datetime_input.month
+    date = datetime_input.day
+    day_of_week = datetime_input.weekday()
+    hour = datetime_input.hour
+    minutes = datetime_input.minute
+
+    # Make a prediction when the user clicks the 'Predict' button
+    if st.button("Predict"):
+        # Prepare the input data as a DataFrame
+        input_data = pd.DataFrame({
+            "Year": [year],
+            "Month": [month],
+            "Date": [date],
+            "Day_of_week": [day_of_week],
+            "Hour": [hour],
+            "Minutes": [minutes],
+            "Temperature": [temperature],
+            "Is_weekend": [is_weekend],
+            "Is_holiday": [is_holiday],
+            "Is_start_of_semester": [is_start_of_semester],
+            "Is_during_semester": [is_during_semester]
+        })
+
+        # Use model to make predictions
+        prediction = model.predict(input_data)
+
+        rounded = str(np.round(prediction[0])).rstrip('.0')
+
+        st.write(f"Predicted Attendance: {rounded} People")
 
 
-# Depending on the selected option, display the corresponding section
-if option == "Batch Prediction":
-    st.header("Batch Prediction")
-    uploaded_file = st.file_uploader("Upload a CSV file for batch predictions:", type=["csv"])
-
-    if uploaded_file is not None:
-        data = pd.read_csv(uploaded_file)
-        st.write("Data Preview:")
-        st.write(data.head())
-
-        # Perform batch predictions here if needed
-        # You can loop through rows in the uploaded CSV and apply predict_attendance to each row
-
-elif option == "Single Row Prediction":
-    st.header("Single Row Prediction")
-    date = st.date_input("Date:")
-    time = st.time_input("Time:")
-    temperature = st.number_input("Temperature (in degrees Celsius):")
-    is_weekend = st.checkbox("Is Weekend?")
-    is_holiday = st.checkbox("Is Holiday?")
-
-    # Make Prediction Button for Single Row
-    single_row_prediction_button = st.button("Predict Attendance (Single Row)")
-
-    if single_row_prediction_button:
-        input_data = {
-            "date": date,
-            "time": time,
-            "temperature": temperature,
-            "is_weekend": is_weekend,
-            "is_holiday": is_holiday,
-        }
-
-        # prediction = predict_attendance(input_data)  # Adjust your prediction function accordingly
-
-        # # Display the prediction
-        # st.subheader("Prediction (Single Row):")
-        # st.write(f"Predicted Attendance: {prediction}")
+if __name__ == "__main__":
+    main()
